00c9ed256bd16e0e1bb695c339295252c5b0d734
Henriette <henriette.behr@web.de>
2020-06-14 14:45:25 +0200

    Hopefully fixed the error

--- partly_random_with_paths.py
+++ partly_random_with_paths.py
@@ -87,6 +87,8 @@ class Partly_Random:
         y_data_copy = y_data.copy()
         
         for idx,p in enumerate(paths): 
+            if not (path_indexes[idx][1] in indices and path_indexes[idx][2] in indices)
+                continue
             for i in range(self.config["n"]):
                 new_path = self.randomize_specific_section(np.array(p))
                 temp_W,temp_V = DTW.get_warp_val_mat(new_path)
