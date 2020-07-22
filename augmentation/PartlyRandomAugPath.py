aa3f1bf4eccf18b5dd4213e2979e139d1741e4d7
Henriette <henriette.behr@web.de>
2020-07-22 11:10:51 +0200

    Added comments for two approaches

--- augmentation/PartlyRandomAugPath.py
+++ augmentation/PartlyRandomAugPath.py
@@ -6,7 +6,11 @@ import pandas as pd
 class PartlyRandomAug:
     
     #Class for the partly random approach with the variable config (dict) where:
-    # -n: number of generated paths for each calculated path
+    # -n: number of newly generated paths for each calculated path
+    
+    #ATTENTION: 
+    # This augmentation method assumes that the path between the time series is already calculated. 
+    # 
     
     def __init__(self, n):
         self.config = dict()
@@ -19,18 +23,26 @@ class PartlyRandomAug:
     def randomize_specific_section(self, path):
         path_copy = path.copy()
         end = path_copy.shape[0]
+        
+        # Choose random start and end point
         random_start = np.random.randint(0,end-2)
         random_end =  np.random.randint(random_start+1,end-1)
-    
         end_idx = path_copy[random_end]
         actual = path_copy[random_start]
     
+        # Copy every point from the original path until the start point to the new path
         new_path = path_copy[:random_start]
+        
+        # Add randomly new points to the new path
         while True:
+            # Check if newly added point matches with the end point or the end of the path
             if actual[0] == end_idx[0] and actual[1] == end_idx[1] or (actual[0] == end and actual[1] == end):
                 break
+                
+            # Add the new point to the new path
             new_path = np.concatenate((new_path,np.array([actual])))
         
+            # Check if just one direction is possible
             if actual[0] == end_idx[0] or actual[0] == end:
                 actual[1] = actual[1]+1
                 continue
@@ -38,7 +50,7 @@ class PartlyRandomAug:
             elif actual[1] == end_idx[1] or actual[1] == end:
                 actual[0] = actual[0]+1
                 continue
-            
+            # if not, choose randomly the next point
             random_choice = np.random.randint(0,3)
         
             if random_choice == 0:
@@ -48,36 +60,20 @@ class PartlyRandomAug:
             else:
                 actual[0] = actual[0]+1
                 actual[1] = actual[1]+1
-    
+                
+        # Add the rest of the original path from end point on until the end 
         new_path = np.concatenate((new_path,np.array(path_copy[random_end:])))
     
         return new_path
     
-    def augment_paths(self, data):
-        #number of timeseries
-        count = data.shape[0]
-        length = data.shape[1]
-        new_series_list = []
-        
-        #calculate every path between every two timeseries
-        for i in range(count):
-            for j in range(count):
-                if i == j:
-                    continue
-                
-                d, path = DTW.dtw(np.reshape(data[i], (length,1)),np.reshape(data[j], (length,1)), path = True)
-                
-                for n in range(self.config["n"]):
-                    new_path = self.randomize_specific_section(path)
-                    temp_W,temp_V = DTW.get_warp_val_mat(new_path)
-                    temp_V = np.diag(temp_V[:,0])
-                    new_series = (np.linalg.inv(temp_V)@temp_W)
-                    new_series = new_series@data[i]
-                    new_series_list.append(new_series)
-                    
-        return np.array(new_series_list)
-    
+
     def augment(self, X_data, y_data, indices, paths, path_indexes):
+        #X_data: A numpy array containing the time series
+        #y_data: A numpy array containing the class labels
+        #indices: The indices of the different time series in X_data (regarding to all the available time series)
+        #paths: All the paths belonging to this dataset
+        #path_indexes: Containing triples [class, time series index 1, time series index 2]
+        #to match the paths to the correct time series       
         
         if not(type(X_data) is np.ndarray and  type(y_data) is np.ndarray):
             print("Input type has to be np.ndarray")
@@ -86,17 +82,23 @@ class PartlyRandomAug:
         X_data_copy = X_data.copy()
         y_data_copy = y_data.copy()
         
+        # iterate through all the paths available
         for idx,p in enumerate(paths): 
+            # check if both time series of the path are existing in X_data
             if not (path_indexes[idx][1] in indices and path_indexes[idx][2] in indices):
                 continue
+            # create for this path n times new paths
             for i in range(self.config["n"]):
+                # create new path
                 new_path = self.randomize_specific_section(np.array(p))
+                # calculate the new time series and add it to X_data and y_data
                 temp_W,temp_V = DTW.get_warp_val_mat(new_path)
                 temp_V = np.diag(temp_V[:,0])
                 new_series = (np.linalg.inv(temp_V)@temp_W)
                 data = X_data[indices == path_indexes[idx][1]]
                 c = path_indexes[idx][0]
                 new_series = new_series@data[0]
+                
                 X_data_copy = np.vstack((X_data_copy, new_series))
                 y_data_copy = np.hstack((y_data_copy, c))
             
--- augmentation/PermutationAugPath.py
+++ augmentation/PermutationAugPath.py
@@ -20,6 +20,9 @@ class PermutationAug:
     
     
     def permutate(self,path):
+        
+        # initialize the variables
+        
         path_copy = path.copy()
         prob = self.config["probability"]
         
@@ -31,22 +34,29 @@ class PermutationAug:
         end = path_copy[-1][0]
         
         while curr[0] != end or curr[1] != end:  
+            # Check if the algorithm is currently in the state "permutate"
             if not permutate:
-           
+                # Choose if the algorithm should permutate with probability prob
                 choice = np.random.choice(2, 1, p=[1.0-prob, prob])
         
+                # if choice is false or the path has just one direction to go
                 if choice[0] == 0 or end in curr:
                     new_path = np.vstack((new_path,np.array(curr)))
                     # get next element of path
                     prev = curr.copy()
                 
+                    # get the index of the current point
                     temp1 = np.where(path_copy[:,0] == curr[0])[0]
                     temp2 = np.where(path_copy[:,1] == curr[1])[0]
                 
+                    # get the next point of the path
                     curr = path_copy[np.intersect1d(temp1,temp2)+1][0]
                 
+                # if choice is true, change the state permutate to True
                 else: 
                     permutate = True
+                    
+                    # permutate the next point and check if the new point does not match with the real point
                     while True:
                         random_choice = np.random.randint(0,3)
                         permutation = prev.copy()
@@ -63,8 +73,10 @@ class PermutationAug:
                         if permutation[0] != curr[0] or permutation[1] != curr[1]:
                             break
             else:
+                # Add the new permutation point
                 new_path = np.vstack((new_path,np.array(permutation)))
             
+                # Check if just one direction is possible
                 if permutation[0] == end:
                     permutation[1] = permutation[1]+1
             
@@ -72,6 +84,7 @@ class PermutationAug:
                     permutation[0] = permutation[0]+1
             
                 else:
+                    # Otherwise: Add a random point
                     random_choice = np.random.randint(0,3)
                     if random_choice == 0:
                         permutation[0] = permutation[0]+1       
@@ -81,7 +94,7 @@ class PermutationAug:
                     else:
                         permutation[0] = permutation[0]+1
                         permutation[1] = permutation[1]+1
-                    
+                # Check if the new point is also in the original point, change the state permutate to False if so    
                 test = (path_copy[:,0] == permutation[0]) & (path_copy[:,1] == permutation[1])
                 if path_copy[test].shape[0] != 0:
                     permutate = False
@@ -92,6 +105,12 @@ class PermutationAug:
     
     
     def augment(self, X_data, y_data, indices, paths, path_indexes):
+        #X_data: A numpy array containing the time series
+        #y_data: A numpy array containing the class labels
+        #indices: The indices of the different time series in X_data (regarding to all the available time series)
+        #paths: All the paths belonging to this dataset
+        #path_indexes: Containing triples [class, time series index 1, time series index 2]
+        #to match the paths to the correct time series       
         
         if not(type(X_data) is np.ndarray and  type(y_data) is np.ndarray):
             print("Input type has to be np.ndarray")
@@ -99,18 +118,23 @@ class PermutationAug:
         
         X_data_copy = X_data.copy()
         y_data_copy = y_data.copy()
-        
+        # iterate through all the paths available
         for idx,p in enumerate(paths): 
+            # check if both time series of the path are existing in X_data
             if not (path_indexes[idx][1] in indices and path_indexes[idx][2] in indices):
-                continue            
+                continue      
+            # create for this path n times new paths
             for i in range(self.config["n"]):
+                # create new path
                 new_path = self.permutate(np.array(p))
+                # calculate the new time series and add it to X_data and y_data
                 temp_W,temp_V = DTW.get_warp_val_mat(new_path)
                 temp_V = np.diag(temp_V[:,0])
                 new_series = (np.linalg.inv(temp_V)@temp_W)
                 data = X_data[indices == path_indexes[idx][1]]
                 c = path_indexes[idx][0]
                 new_series = new_series@data[0]
+                
                 X_data_copy = np.vstack((X_data_copy, new_series))
                 y_data_copy = np.hstack((y_data_copy, c))
             
